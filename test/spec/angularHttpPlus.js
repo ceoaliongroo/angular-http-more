70846d703371d978ab4c4b0d3fb73724a9f0be77
Carlos Mantilla <cmantilla@aliongroo.com>
2015-02-04 10:49:48 +0200

    WIP: testing module defined, if is extendable and if the configuration is set.

--- test/spec/angularHttpPlus.js
+++ test/spec/angularHttpPlus.js
@@ -1,34 +1,40 @@
 'use strict';
 
-describe('Service: angularHttpPlus', function () {
+describe('Service: Restful', function () {
 
   // load the service's module
   beforeEach(module('elementModule'));
 
   // instantiate service
-  var testService;
+  var restfulService;
+  var resourceService = {};
   // mock options, configuration fo server request.
   var options;
 
   beforeEach(inject(function (_Restful_) {
-    testService = _Restful_;
+    restfulService = _Restful_;
+    // Extend the service.
+    extend(resourceService, restfulService);
 
     options = {
       url: 'http://server.com/api',
       transformResponse: angular.noop
     }
 
-
   }));
 
-  it('should to be define Restful', function () {
-    expect(!!testService).toBe(true);
+  it('should be defined', function () {
+    expect(!!restfulService).toBe(true);
+  });
+
+  it('should be extendable to other services', function () {
+    expect(!!resourceService.setConfig).toBeDefined(true);
   });
 
   // Configuration of the service.
-  it('should to configured the service to return data from the server.', function () {
-    testService.get(options);
-    expect(testService.options.url).toContain('http://server.com/api');
+  it('should to set options', function (resourceService) {
+    resourceService.setOptions(options);
+    expect(resourceService.get()).toContain('http://server.com/api');
   });
 
 });
